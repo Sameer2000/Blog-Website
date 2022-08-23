64d2423365e9d1445e376e407cfa88df1e8fe143
Sameer <sameerofficial895@gmail.com>
2022-08-23 20:28:13 +0530

    Add MongoDB database

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
@@ -7,53 +8,73 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-let posts = [];
-
 const app = express();
 
-app.set('view engine', 'ejs'); //h
+app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-app.get('/', function(req, res){
-  res.render('home', {startingContent: homeStartingContent, newPosts: posts});
+mongoose.connect('mongodb://localhost:27017/blogDB', {
+  useNewUrlParser: true
 });
 
-app.get('/about', function(req, res){
-  res.render('about', {startingContent: aboutContent});
+const postsSchema = new mongoose.Schema({
+  title: String,
+  body: String
 });
 
-app.get('/contact', function(req, res){
-  res.render('contact', {startingContent: contactContent});
+const Post = mongoose.model('Post', postsSchema);
+
+app.get('/', function (req, res) {
+  Post.find({}, function (err, posts) {
+    res.render('home', {
+      startingContent: homeStartingContent,
+      newPosts: posts
+    });
+  });
 });
 
-app.get('/compose', function(req, res){
+app.get('/about', function (req, res) {
+  res.render('about', {
+    startingContent: aboutContent
+  });
+});
+
+app.get('/contact', function (req, res) {
+  res.render('contact', {
+    startingContent: contactContent
+  });
+});
+
+app.get('/compose', function (req, res) {
   res.render('compose');
 });
 
-app.post('/compose', function(req, res){
-  const post = {
+app.post('/compose', function (req, res) {
+  const post = new Post({
     title: req.body.postTitle,
     body: req.body.postBody
-  };
-  posts.push(post);
-  res.redirect('/');
-});
-
-app.get('/posts/:postName', function(req, res){
-  posts.forEach(function(post){
-    if(_.lowerCase(post.title) === _.lowerCase(req.params.postName)){
-      res.render('post', {title: post.title, content: post.body});
-    }else{
-      console.log('No Match Found!');
+  });
+  post.save(function(err){
+    if(!err){
+      res.redirect('/');
     }
   });
 });
 
+app.get('/posts/:postId', function (req, res) {
+  const requestedPostId = req.params.postId;
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render('post', {
+      title: post.title,
+      content: post.body
+    });
+  });
+});
 
-
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("App is live on http://localhost:3000");
 });
